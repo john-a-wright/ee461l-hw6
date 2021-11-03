d60bac480742bfcb132a5dade5bbcc4be1369d25
john-a-wright <johnawright@utexas.edu>
2021-11-03 14:37:09 -0500

    try 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
+  const [name, setName] = React.useState("");
+  const [output, setOutput] = React.useState("");
+
+  const handleSubmit = () => {
+    const url = "http://127.0.0.1:5000/" + name;
+    fetch(url)
+      .then((res) => {
+        if (res.ok) {
+          setOutput(res);
+          console.log(res);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Enter first name</p>
+        <input onChange={(event) => setName(event.target.value)} />
+        <button type="button" onClick={handleSubmit}>
+          Submit
+        </button>
+        <p>{output}</p>
       </header>
     </div>
   );
