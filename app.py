24ddebdbdbc7d9103260ce9f0f1eedf8f19ca280
john-a-wright <johnawright@utexas.edu>
2022-03-30 18:16:10 -0500

    jeesoo test3

--- app.py
+++ app.py
@@ -2,24 +2,29 @@ from flask import Flask, jsonify
 from flask.helpers import send_from_directory
 from flask_cors import CORS
 
+# Location for index.html
 app = Flask(__name__, static_folder="frontend/build", static_url_path="")
 
 CORS(app)
 
-@app.route('/<string:firstName>', methods=['GET'])
-def find_name(firstName: str):
-    print(firstName)
-    if firstName == "Jeesoo":
-        output = "Min"
+# Get the name from the url and return the output
+@app.route('/<string:name>',methods=['GET'])
+def output_name(name):  # put application's code here
+    if name=='Jeesoo':
+        return jsonify(
+            status=200,
+            message="Min"
+        )
     else:
-        output = "User Not Found"
-    return jsonify(lastName=output)
+        return jsonify(
+            status=404,
+            message="User Not Found"
+        )
 
-
+# Starting route for index
 @app.route("/")
 def index():
     return send_from_directory(app.static_folder, "index.html")
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,34 @@
 import "./App.css";
-import MyComponent from "./MyComponent";
+import React from "react";
 
+// Holds the webpage frontend
 function App() {
+  const [name, setName] = React.useState("");
+  const [output, setOutput] = React.useState("");
+
+  // Function for handling button click
+  const handleSubmit = () => {
+    const url = "/" + name;
+    console.log(url);
+    fetch(url)
+      .then((response) => response.json())
+      .then((jsondata) => setOutput(jsondata["message"]))
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  // Returning JS for the formatting and display
   return (
     <div className="App">
-      <MyComponent />
+      <header className="App-header">
+        <p>Please enter first name</p>
+        <input onChange={(event) => setName(event.target.value)} />
+        <button type="button" onClick={handleSubmit}>
+          Find last name
+        </button>
+        <p>{output}</p>
+      </header>
     </div>
   );
 }
